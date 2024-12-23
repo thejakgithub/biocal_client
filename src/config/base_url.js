c7b7d68525aa26730b6669894dd084a2f312e142
Switt Chasri <jak171646167@gmail.com>
2024-12-23 17:04:40 +0700

    Update base_url.js

--- src/config/base_url.js
+++ src/config/base_url.js
@@ -1,5 +1,5 @@
 //  host api
- const base_url = "http://localhost:4000";
+ const base_url = "https://biocal-fd1b864f2b3e.herokuapp.com";
 // const base_url = "https://biocal-th.herokuapp.com"
 export default base_url;
 
